d75a77c50d24229924e012d02b52ce72515576f2
seanm00n <deccj97@naver.com>
2022-12-06 01:50:44 +0900

    d
    feat
    - clear.widzet : set score text

---
+++ Source/PortalEX/Public/Score/CustomDataTables.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Engine/DataTable.h"
+#include "CustomDataTables.generated.h"
+USTRUCT(BlueprintType)
+struct FScoreTableRow : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Score")
+		int32 BScore;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Score")
+		int32 CScore;
+};
+UCLASS()
+class PORTALEX_API ACustomDataTables : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACustomDataTables();
+
+//protected:
+//	// Called when the game starts or when spawned
+//	virtual void BeginPlay() override;
+//
+//public:	
+//	// Called every frame
+//	virtual void Tick(float DeltaTime) override;
+
+};
